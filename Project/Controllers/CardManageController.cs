e2b1ac42e45ad7a70f07442375f85174712dcd39
LAPTOP-56BQ36IL\sposnny <sposnny@gmail.com>
2024-07-27 15:29:06 +0800

    1.新增AddCard.cshtml / EditCard,cshtml / CardManage.cshtml 頁面 2.建立CardManage.cshtml 資料來源 3.串接管理頁面及各功能頁面

--- Project/Controllers/CardManageController.cs
+++ Project/Controllers/CardManageController.cs
@@ -1,12 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PTCGProject.Models;
+using PTCGProject.Service;
+using System.Data;
 
 namespace PTCGProject.Controllers
 {
     public class CardManageController : Controller
     {
+        private CardService _CardService;
+        public CardManageController(IDbConnection dbConnection)
+        {
+            _CardService = new CardService(dbConnection);
+        }
+
         public IActionResult CardManage()
         {
-            return View();
+            List<CardModel> InitCards = _CardService.GetCardsDetail();
+            return View(InitCards);
+        }
+
+        [HttpGet]
+        public IActionResult EditCard(CardModel Card)
+        {
+            return View(Card);
+        }
+
+        [HttpPost]
+        public IActionResult EditCard(string id,string version)
+        {
+            CardModel Card = new CardModel();
+            Card.CardId = id;
+            Card.CardVersion = version;
+
+            Card = _CardService.GetCardsDetail(Card).FirstOrDefault();
+
+            return Json(new { redirectUrl = Url.Action("EditCard", "CardManage", Card) });
+        }
+
+        [HttpPost]
+        public IActionResult Update(CardModel Card) 
+        {
+
+            return RedirectToAction("CardManage", "CardManage");
+        }
+
+        [HttpGet]
+        public IActionResult AddCard()
+        {
+            CardModel Card = new CardModel();
+            return View(Card);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CardModel Card)
+        {
+            return View(Card);
+        }
+
+        public IActionResult DeleteCard()
+        {
+            return RedirectToAction("CardManage", "CardManage");
         }
     }
 }
