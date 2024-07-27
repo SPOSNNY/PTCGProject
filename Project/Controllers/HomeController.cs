99753399943fc2830c0362c34d74337819bd948c
LAPTOP-56BQ36IL\sposnny <sposnny@gmail.com>
2024-07-27 12:44:12 +0800

    1.實作首頁查詢卡牌功能

--- Project/Controllers/HomeController.cs
+++ Project/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Project.Models;
+using PTCGProject.DataProcess;
 using PTCGProject.Models;
+using PTCGProject.Service;
+using System;
 using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
@@ -10,16 +13,34 @@ namespace Project.Controllers
 {
     public class HomeController : Controller
     {
-        private IDbConnection _dbConnection;
+        private CardService _CardService;
         public HomeController(IDbConnection dbConnection)
         {
-            _dbConnection = dbConnection;
+            _CardService = new CardService(dbConnection);
         }
 
         public IActionResult Index()
         {
-            List<CardModel> cards =  _dbConnection.Query<CardModel>("select * from PKM_DETAIL").ToList();
+            List<CardModel> InitCards = Init();
+            return View(InitCards);
+        }
+        
+        [HttpPost]
+        public IActionResult Index(CardModel cardModel)
+        {
+            List<CardModel> cards = _CardService.GetCardsDetail(cardModel.CardAttribute, cardModel.CardType, cardModel.CardRarity, cardModel.CardVersion, cardModel.CardName);
+            Init();
             return View(cards);
+        }
+
+        private List<CardModel> Init()
+        {
+            List<CardModel> InitCards = _CardService.GetCardsDetail();
+            ViewBag.CardAttributes = InitCards.Select(c => c.CardAttribute).Distinct();
+            ViewBag.CardTypes = InitCards.Select(c => c.CardType).Distinct();
+            ViewBag.CardRarities = InitCards.Select(c => c.CardRarity).Distinct();
+            ViewBag.CardVersions = InitCards.Select(c => c.CardVersion).Distinct();
+            return InitCards;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
